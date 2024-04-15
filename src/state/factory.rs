83271f35de23642ef9a0d460195b26f3f0a92140
Roberto Previdi <hariseldon78@gmail.com>
2024-04-15 03:49:58 +0200

    factory works! compare fails :(

--- src/state/factory.rs
+++ src/state/factory.rs
@@ -13,11 +13,11 @@ pub trait Process {
     fn run(&self,state:&mut State);
 }
 
-struct Factory; 
+pub struct Factory; 
 impl Factory {
     pub fn create_node(node_name:String, config: &Value) -> Box<dyn Process> {
         match config["driver"].as_str() {
-            Some("input::mysql") => Box::new(InputMysql::from_config(node_name.clone(), config)),
+            Some("input::mysql") => Box::new(InputMysql::from_config(node_name, config)),
             Some("output::sql-inserts") => Box::new(OutputSqlInserts::from_config(node_name, config)),
             Some("output::compare") => Box::new(OutputCompare::from_config(node_name, config)),
             _ => panic!("Unknown driver: {}", config["driver"]),
